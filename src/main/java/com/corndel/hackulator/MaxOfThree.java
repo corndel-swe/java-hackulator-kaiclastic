0307c5a946ea35fb21001e1011c1bbfdc31fa124
kaiclastic <halajash567@outlook.com>
2024-09-17 14:29:40 +0100

    fixes max of three method

--- src/main/java/com/corndel/hackulator/MaxOfThree.java
+++ src/main/java/com/corndel/hackulator/MaxOfThree.java
@@ -1,8 +1,14 @@
 package com.corndel.hackulator;
-
+import java.util.Arrays;
 public class MaxOfThree {
 
   static int maxOfThree(int x, int y, int z) {
+    
+    
+
+    if (x==y && x==z){
+      return x;
+    }
     if (x > y && x > z) {
       return x;
     }
@@ -13,8 +19,12 @@ public class MaxOfThree {
 
     if (z > x && z > y) {
       return z;
+    } else  {
+      int [] numberList ={x,y,z};
+      Arrays.sort(numberList);
+      return numberList[2];
     }
 
-    throw new Error("Is this reachable?");
+    // throw new Error("Is this reachable?");
   }
 }
